10ec47815f8da4da5c25f0425a5ad9e70566ed33
Owen, Nate <nowen@jjkeller.com>
2017-01-23 13:11:51 -0600

    sass, normailze

--- src/spa/app.js
+++ src/spa/app.js
@@ -2,5 +2,6 @@ import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Increment from './increment.react';
+import './normalize.scss';
 
 ReactDOM.render(<Increment />, document.getElementById('root'));
